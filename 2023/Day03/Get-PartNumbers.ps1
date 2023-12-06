ff9b221c947878f6616ee85a4cba344a2ee5f57e
Mark Phillips <mark_phillips85@hotmail.com>
2023-12-06 12:53:15 +0000

    [AoC-D03] Part 2 solution

--- 2023/Day03/Get-PartNumbers.ps1
+++ 2023/Day03/Get-PartNumbers.ps1
@@ -13,9 +13,6 @@ function Get-PartNumbersSumP1 {
     )
     
     $PartNumbersSum = 0
-
-    $Content | Select-Object -First 14 | Write-Verbose; Write-Verbose "..."
-
     $YLength = $Content.Length
     $XLength = $Content[0].Length
 
@@ -64,9 +61,67 @@ function Get-PartNumbersSumP1 {
 }
 
 
+
+function Get-GearRatioSumP2 {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]]
+        $Content
+    )
+    
+    $GearRatioSum = 0
+    $YLength = $Content.Length
+    $XLength = $Content[0].Length
+
+    for ($y = 0; $y -lt $YLength; $y++) {
+        for ($x = 0; $x -le $XLength; $x++) {
+            $char = $Content[$y][$x]
+            
+            if ($char -eq "*") {
+                $numbers = $null
+                for ($y2 = ($y - 1); $y2 -le ($y + 1); $y2++) {
+                    if ($y2 -lt 0 -or $y2 -ge $YLength) { continue }
+                    $numberLine = $null
+
+                    # Look to left of X coordinate
+                    $i = 1
+                    $num = $Content[$y2][$x-$i]
+                    while ($num -like "[0-9]") {
+                        $numberLine = $num + $numberLine
+                        $num = $Content[$y2][$x-(++$i)]
+                    }
+
+                    # Take any number on the X coordinate
+                    $numberLine += $Content[$y2][$x] -replace "[^0-9]", " "
+
+                    # Look to right of X coordinate
+                    $i = 1
+                    $num = $Content[$y2][$x+$i]
+                    while ($num -like "[0-9]") {
+                        $numberLine = $numberLine + $num
+                        $num = $Content[$y2][$x+(++$i)]
+                    }
+
+                    $numbers += " " + $numberLine
+                }
+                $numbers = ($numbers -replace " +", " ").Trim().Split(" ")
+                Write-Verbose "Numbers: $numbers"
+
+                if ($numbers.Count -eq 2) {
+                    $GearRatioSum += [int]$numbers[0] * [int]$numbers[1]
+                }
+            }
+        }
+    }
+
+    $GearRatioSum
+}
+
+
 Write-Verbose "Path: $Path"
 
 if (-not([string]::IsNullOrEmpty($Path))) {
     $Content = Get-Content -Path $Path
     Get-PartNumbersSumP1 -Content $Content
+    Get-GearRatioSumP2 -Content $Content
 }
