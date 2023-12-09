bd14b0f29a1cb18513fe505c3785399da058a755
Mark Phillips <mark_phillips85@hotmail.com>
2023-12-09 09:46:21 +0000

    [Aoc-D05] Part 1 solution

--- 2023/Day05/Get-SeedDetails.Tests.ps1
+++ 2023/Day05/Get-SeedDetails.Tests.ps1
@@ -6,14 +6,14 @@ BeforeAll {
         "seeds: 79 14 55 13",
         "",
         "seed-to-soil map:",
-        "50 98 2",
-        "52 50 48",
-        "",
-        "soil-to-fertilizer map:",
-        "0 15 37",
-        "37 52 2",
-        "39 0 15",
-        "",
+        "50 98 2",  # Destination Source Length e.g. 98, 99 --> 50, 51. Shift is 98 to 50 = -48.
+        "52 50 48", # Soil        Seed   Length e.g. 50, 51 --> 52, 53. Shift is 50 to 52 = +2.
+        "",         #                          Rest: 0, 1   --> 0, 1.   Shift is 0 to 0   = +0.
+        "soil-to-fertilizer map:",  # Mapping:  Seed --> Soil
+        "0 15 37",                  #          0..49 --> 0..49    <-- Default is 1:1 mapping      : 0..49  --> 0
+        "37 52 2",                  #         50..97 --> 52..99   <-- Special case - shift by +2  : 50..97 --> 2
+        "39 0 15",                  #         98..99 --> 50..51   <-- Special case - shift by -48 : 98..99 --> -48
+        "",                         #        100..on --> 100..on  <-- Default is 1:1 mapping      : 100..  --> 0
         "fertilizer-to-water map:",
         "49 53 8",
         "0 11 42",
@@ -37,23 +37,58 @@ BeforeAll {
         "60 56 37",
         "56 93 4"
     )
+
+    Write-Verbose "SeedMapData: $($SeedMapData | Out-String)"
 }
 
 Describe "Day 05" {
     Context "Part 1" {
         It "Should return the correct seed details" {
             # Arrange
-            $SeedMap = $SeedMapData
+            $SeedMap = $SeedMapData | Where-Object { [string]::IsNullOrEmpty($_) -eq $false }
 
             # Act
-            $SeedMinLocation = Get-SeedMinLocationP1 -SeedMap $SeedMap
+            $SeedMinLocation = Get-SeedMinLocationP1 -SeedMapData $SeedMap
 
-            # Assert
+            # # Assert
             $SeedMinLocation | Should -Be 35
+        }
+
+        It "Large numbers should work" {
+            $SeedMapData = (
+                "seeds: 565778304 341771914",
+                "seed-to-soil map:",
+                "1136439539 28187015 34421000",
+                "soil-to-fertilizer map:",
+                "2997768542 2385088490 141138894",
+                "fertilizer-to-water map:",
+                "1539871014 1431400479 38399903",
+                "water-to-light map:",
+                "1509583382 1639808290 20361832",
+                "light-to-temperature map:",
+                "3498288578 2645051323 42074132",
+                "temperature-to-humidity map:",
+                "1130946446 972737563 146373650",
+                "humidity-to-location map:",
+                "3903940466 3635148971 125939893"
+            )
+
+            $SeedMinLocation = Get-SeedMinLocationP1 -SeedMapData $SeedMapData
+
+            $SeedMinLocation | Should -Be 1062922501
         }
     }
 
     Context "Part 2" {
+        It "Should return something" -Pending {
+            # Arrange
+            $SeedMap = $SeedMapData
 
+            # Act
+            $SeedMinLocation = Get-SeedMinLocationP2 -SeedMap $SeedMap
+
+            # Assert
+            $SeedMinLocation | Should -Be 35
+        }
     }
 }
--- 2023/Day05/Get-SeedDetails.ps1
+++ 2023/Day05/Get-SeedDetails.ps1
@@ -13,10 +13,55 @@ function Get-SeedMinLocationP1 {
         $SeedMapData
     )
     
-    # TBD
+    # $SeedMapData
+    $AllMaps = @{}
+    for ($i = 0; $i -lt $SeedMapData.Length; $i++) {
+        $Line = $SeedMapData[$i]
+        if ($Line -match "seeds:") {
+            $Seeds = ($Line -replace "seeds: ", "") -split "\s"
+        }
+
+        if ($Line -match " map:") {
+            $MapName = ($Line -replace " map:", "")
+            $Line = $SeedMapData[$i + 1]
+            $Map = [System.Collections.ArrayList]@()
+            while ($Line -match '\d+ \d+ \d+' -and $i -lt $SeedMapData.Length) {
+                $i++
+                $Destination, $SourceStart, $Length = ($Line -split "\s")
+                $SourceEnd = [Int64]$SourceStart + [Int64]$Length - 1
+                $Shift = [Int64]$Destination - [Int64]$SourceStart
+
+                $null = $Map.Add([PSCustomObject]@{
+                    SourceStart = $SourceStart -as [Int64]
+                    SourceEnd = $SourceEnd -as [Int64]
+                    Shift = $Shift -as [Int64]
+                })
+
+                $Line = $SeedMapData[$i + 1]
+            }
+            $AllMaps.Add($MapName, $Map)
+        }
+    }
+
+    $SeedToLocationMap = @{}
+    foreach ($Seed in $Seeds) {
+        $MapNamePart = "seed-to-"
+        $MappedValue = $Seed -as [Int64]
+        while ($AllMaps.Keys -like "$MapNamePart*") {
+            $Shift = 0
+            $MapName = $AllMaps.Keys | Where-Object { $_ -match $MapNamePart }
+            $Map = $AllMaps[$MapName]
+            $Shift = $Map | Where-Object { $_.SourceStart -le $MappedValue -and $_.SourceEnd -ge $MappedValue } | Select-Object -ExpandProperty Shift
+            $MappedValue += [Int64]$Shift
+            $MapNamePart = $MapName -replace "$MapNamePart", "" -replace '$', '-to-'
+        }
+        $SeedToLocationMap.Add($Seed, $MappedValue)
+    }
+
+    return $SeedToLocationMap.Values | Measure-Object -Minimum | Select-Object -ExpandProperty Minimum
 }
 
 # Main
 if (Test-Path -Path $Path) {
-    Get-SeedMinLocationP1 -SeedMap (Get-Content -Path $Path)
+    Get-SeedMinLocationP1 -SeedMap (Get-Content -Path $Path | Where-Object { [string]::IsNullOrEmpty($_) -eq $false} )
 }
